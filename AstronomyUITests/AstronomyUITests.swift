a3c38b179d66973f89a78bc47bc1026e400e4701
Craig Swanson <cswanson02@me.com>
2020-02-06 13:01:02 -0600

    Added test to save photo from detail view controller.

--- AstronomyUITests/AstronomyUITests.swift
+++ AstronomyUITests/AstronomyUITests.swift
@@ -10,22 +10,29 @@ import XCTest
 
 class AstronomyUITests: XCTestCase {
 
+    let app = XCUIApplication()
+
     override func setUp() {
-        let app = XCUIApplication()
+        app.launch()
         app.launchArguments = ["UITesting"]
     }
 
     // at least 4 tests.
     // test saving a photo
     // test viewing another sol (14-16)
-    func testExample() {
+    func testSavePhoto() {
 
-        
-        let app = XCUIApplication()
-        app.collectionViews.children(matching: .cell).element(boundBy: 3).images["CollectionView.Image"].tap()
+        app.collectionViews.children(matching: .cell).element(boundBy: 0).images["CollectionView.Image"].tap()
         app/*@START_MENU_TOKEN@*/.buttons["PhotoDetailViewController.SaveButton"]/*[[".buttons[\"Save to Photo Library\"]",".buttons[\"PhotoDetailViewController.SaveButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+/*
+         NOTE: upon first run, tapping on save image brings the alert to allow access to photo library
+         attempted to automate with this code, but it did not work
+        let photoLibraryAccessAlert = app.alerts["“Astronomy” Would Like to Access Your Photos"].scrollViews.otherElements.buttons["OK"]
+        if photoLibraryAccessAlert.exists {
+            photoLibraryAccessAlert.tap()
+            app/*@START_MENU_TOKEN@*/.buttons["PhotoDetailViewController.SaveButton"]/*[[".buttons[\"Save to Photo Library\"]",".buttons[\"PhotoDetailViewController.SaveButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        }
+ */
         app.alerts["Photo Saved!"].scrollViews.otherElements.buttons["Okay"].tap()
-        
-
     }
 }
