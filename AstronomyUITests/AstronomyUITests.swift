38ae8d980ca246bc1bedd52d022885ef16eb4ddb
Craig Swanson <cswanson02@me.com>
2020-02-06 22:11:47 -0600

    Added test for first run of app and allowing access to the photo library.

--- AstronomyUITests/AstronomyUITests.swift
+++ AstronomyUITests/AstronomyUITests.swift
@@ -22,6 +22,24 @@ class AstronomyUITests: XCTestCase {
     // test viewing another sol (14-16)
     // test sol label after changing dates
     // maybe test photo detail date by comparing it to sol date from table view?
+    
+    func testPermitPhotoLibraryAccess() {
+        // Only returns true on first run of app when system request photo library access
+        
+        app.collectionViews.children(matching: .cell).element(boundBy: 6).images["CollectionView.Image"].tap()
+        app/*@START_MENU_TOKEN@*/.buttons["PhotoDetailViewController.SaveButton"]/*[[".buttons[\"Save to Photo Library\"]",".buttons[\"PhotoDetailViewController.SaveButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        addUIInterruptionMonitor(withDescription: "System Dialog") { (alert) -> Bool in
+        let photoLibraryPermissionText = "Would Like to Access Your Photos"
+            if alert.label.contains(photoLibraryPermissionText) {
+                XCTAssertTrue(alert.exists)
+                alert.buttons["OK"].tap()
+        }
+            return true
+        }
+        app.tap()
+    }
+    
     func testSavePhoto() {
 
         app.collectionViews.children(matching: .cell).element(boundBy: 6).images["CollectionView.Image"].tap()
